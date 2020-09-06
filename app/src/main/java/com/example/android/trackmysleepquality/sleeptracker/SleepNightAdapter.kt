9704ac0b47f83838488e329b204c330704273634
JayTalekar <jaytalekar.developer@gmail.com>
2020-09-07 00:55:53 +0530

    Refactored to encapsulate binding and inflating code in ViewHolder class.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -43,38 +44,61 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>(){
     // and ViewHolder of which data is to be set
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
-
-        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(item)
     }
+
 
     //Called when RecyclerView needs a new ViewHolder of the given type
     //The method must inflate the required view and then wrap it with respective ViewHolder
     // and then return it
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        //inflater to inflate the required view
-        val layoutInflater = LayoutInflater.from(parent.context)
-        //Inflating the required view by instantiating the XML File
-        val view = layoutInflater.inflate(R.layout.list_item_sleep_night,
-                                                        parent, false)
-        //Wrapping the view with its respective holder
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 
 
-    class ViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView : View): RecyclerView.ViewHolder(itemView){
         val sleepLength : TextView = itemView.findViewById(R.id.sleep_length)
         val quality : TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage : ImageView = itemView.findViewById(R.id.quality_image)
+
+        /**
+         *  Binds the data from SleepNight instance to bind it on a List ItemView
+         *  @param item Takes SleepNight instance for accessing and converting its data
+         */
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(item.sleepQuality, res)
+
+            qualityImage.setImageResource(when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            })
+        }
+
+
+        companion object {
+
+            /**
+             * Encapsulates the behaviour of Inflating the layout
+             * @return Returns a ViewHolder with an inflated View
+             */
+            fun from(parent: ViewGroup): ViewHolder {
+                //inflater to inflate the required view
+                val layoutInflater = LayoutInflater.from(parent.context)
+                //Inflating the required view by instantiating the XML File
+                val view = layoutInflater.inflate(R.layout.list_item_sleep_night,
+                        parent, false)
+                //Wrapping the view with its respective holder
+                return ViewHolder(view)
+            }
+        }
+
     }
+
 }
